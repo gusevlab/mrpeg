d3bd07c80ba9409519c295a761d99fe9c629bc15
Zeyun <zeyunlu@usc.edu>
2025-01-27 02:25:15 -0500

    update

--- mrpeg/peg.py
+++ mrpeg/peg.py
@@ -523,6 +523,7 @@ def infer_peg(
     inv_dvd = mat_inv_se @ inv_ld @ mat_inv_se
 
     gamma, gamma_se = _mrld(beta, X, inv_dvd)
+    import pdb; pdb.set_trace()
     gamma_p = 2 * t.sf(jnp.abs(gamma / gamma_se), beta.shape[0] - 1)
     
     log.logger.info(f"Starting permutation test with {perm_number} times.")
